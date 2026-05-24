df49ea529e2be39456ea49ac1c335ef00c7d6910
prashantmganiger <prashant.m.ganiger007@gmail.com>
2026-05-24 16:34:03 +0530

    Update TestingApplication.java

--- src/main/java/com/testing/TestingApplication.java
+++ src/main/java/com/testing/TestingApplication.java
@@ -1,14 +1,13 @@
 package com.testing;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
-public class TestingApplication {
+@RestController
+public class HomeController {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TestingApplication.class, args);
-		System.out.println("Hello World");
-	}
-
+    @GetMapping("/")
+    public String home() {
+        return "Hello World";
+    }
 }
